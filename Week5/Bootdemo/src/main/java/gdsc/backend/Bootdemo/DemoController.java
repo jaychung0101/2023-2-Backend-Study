654ddd3ba9047c55eba7ae9dd342f456e15ffb14
JayChung0101 <chung5587@naver.com>
2023-10-29 23:23:26 +0900

    Week5 과제 제출

---
+++ Week5/Bootdemo/src/main/java/gdsc/backend/Bootdemo/DemoController.java
@@ -0,0 +1,29 @@
+package gdsc.backend.Bootdemo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    private final DemoService demoService;
+
+    @GetMapping("/demoEntities")
+    public ResponseEntity<List<DemoDto>> getDemoEntities() {
+        return ResponseEntity.ok().body(demoService.getDemoEntities());
+    }
+
+    @PostMapping("/demoEntities")
+    public void getDemoEntities(@RequestBody DemoDto demoDto) {
+        demoService.addDemoEntity(demoDto);
+    }
+
+    @GetMapping("/demoEntities/{id}")
+    public DemoDto getDemoEntitiesId(@PathVariable Long id) {
+        return demoService.getDemoEntityName(id);
+    }
+}
